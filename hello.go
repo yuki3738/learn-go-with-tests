b0f00ae88d6f199b296995940edb2dd1950e4922
yuki3738 <y.minamiya@gmail.com>
2023-07-22 19:27:14 +0900

    one...last...refactor?
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#one...last...refactor

--- hello.go
+++ hello.go
@@ -1,30 +1,29 @@
 package main
 
-import "fmt"
-
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+		spanish = "Spanish"
+		french = "French"
+		englishHelloPrefix = "Hello, "
+		spanishHelloPrefix = "Hola, "
+		frenchHelloPrefix = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Spanish":
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
-}
-
-func main() {
-	fmt.Println(Hello("world", ""))
+	return
 }
