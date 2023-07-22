5b3e14ea03f6273e245ba3bc9aa421916c6a771a
yuki3738 <y.minamiya@gmail.com>
2023-07-22 19:17:36 +0900

    French
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#french

--- hello.go
+++ hello.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "Spanish" {
+	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
