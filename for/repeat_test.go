6a90a616e11440766513b4b9e8a856c772731907
yuki3738 <y.minamiya@gmail.com>
2023-07-23 03:50:23 +0900

    Practice exercises
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/iteration#practice-exercises

--- for/repeat_test.go
+++ for/repeat_test.go
@@ -1,9 +1,12 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -13,6 +16,12 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10000)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 3)
+	fmt.Println(repeated)
+	// Output: bbb
 }
