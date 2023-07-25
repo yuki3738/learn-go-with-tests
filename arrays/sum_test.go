4c905861c644ec30e4db58683b8428b53535eb58
yuki3738 <y.minamiya@gmail.com>
2023-07-26 02:27:49 +0900

    Write the minimal amount of code for the test to run and check the failing test output
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-the-minimal-amount-of-code-for-the-test-to-run-and-check-the-failing-test-output-2

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -35,7 +36,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
