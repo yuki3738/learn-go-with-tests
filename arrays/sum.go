233b641472a25ec7138b0b6869eaa92450a1ec7d
yuki3738 <y.minamiya@gmail.com>
2023-07-27 00:50:25 +0900

    Write enough code to make it pass
    https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-enough-code-to-make-it-pass-3

--- arrays/sum.go
+++ arrays/sum.go
@@ -11,7 +11,8 @@ func Sum(numbers []int) int {
 func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
 	}
 
 	return sums
